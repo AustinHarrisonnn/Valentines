858c932204b4d6a3e36491512d2e3c17f897449a
Austin Harrison <austin.harrisonn@yahoo.com>
2025-01-23 00:04:57 -0500

    trying eleven labs api

--- Valentines/src/App.jsx
+++ Valentines/src/App.jsx
@@ -3,11 +3,11 @@ import './App.css';
 import Table from './components/Table';
 
 const App = () => {
-  
+
   return (
     <div className="App">
       <div className="main-header">
-        <h1>Valentine's day is coming up...</h1>
+        <h1>Valentine's day is coming up....</h1>
       </div>
       <div className="main-container">
         <Table />
--- Valentines/src/components/Card.jsx
+++ Valentines/src/components/Card.jsx
@@ -1,6 +1,35 @@
 import React from 'react';
+import { ElevenLabsClient } from "elevenlabs";
+import { Readable } from "stream";
+
+const client = new ElevenLabsClient({
+    apiKey: "sk_00929336286cc49b5a9503e412aa9d9970e50996c4c172d6",
+});
 
 const Card = (props) => {
+
+    const handleYesClicked = async () => {
+        try {
+            const audioStream = await client.textToSpeech.convertAsStream("nPczCjzI2devNBz1zQrb", {
+                text: "Hi!",
+                model_id: "eleven_multilingual_v2",
+                
+            });
+
+            const audioChunks = [];
+            for await (const chunk of audioStream) {
+                audioChunks.push(chunk);
+            }
+            const audioBlob = new Blob(audioChunks, { type: 'audio/mpeg' });
+
+            const audioUrl = URL.createObjectURL(audioBlob);
+            const audio = new Audio(audioUrl);
+            audio.play();
+        } catch (error) {
+            console.error('could not generate audio', error);
+        }
+        };
+
     return (
         <div className="card">
             <div className="head">
@@ -8,7 +37,7 @@ const Card = (props) => {
             </div>
             <div className="body">
                 <h2>Will ju be my valentine?</h2>
-                <button type="button" className="button1">Yes</button>
+                <button type="button" className="button1" onClick={handleYesClicked}>Yes</button>
                 <button type="button" className="button2">No</button>
             </div>
         </div>
