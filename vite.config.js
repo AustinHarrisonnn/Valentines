95948fca02b118cd3f874f25d16a7073af74ae7c
Austin Harrison <austin.harrisonn@yahoo.com>
2025-01-28 12:06:31 -0500

    Fix local dev and prepare for pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/AustinHarrisonnn.github.io-valentines-day/',
+  base: process.env.NODE_ENV === 'production' 
+    ? '/AustinHarrisonnn.github.io-valentines-day/'  
+    : '/',  
   plugins: [react()],
-})
+});
