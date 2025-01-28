9ac6cbdfa0c0a427e57b6179ff036ec694c5d2f9
Austin Harrison <austin.harrisonn@yahoo.com>
2025-01-28 12:35:16 -0500

    fixing issues

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' 
-    ? '/AustinHarrisonnn.github.io-valentines-day/'  
-    : '/',  
+  base: '/AustinHarrisonnn.github.io-valentines-day/', // This is the subfolder on GitHub Pages
   plugins: [react()],
 });
